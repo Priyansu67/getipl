3f9d44e749e681aa04111ebe1d2311fccc89b8b3
Priyansu67 <25521478+Priyansu67@users.noreply.github.com>
2023-03-20 17:33:10 +0530

    Added Upscaling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import './App.css';
-import DragAndDrop from './DragAndDrop'
+import React from "react";
+import "./App.css";
+import DragAndDrop from "./DragAndDrop";
 
 function App() {
   return (
     <div className="App">
-      <h1>IPL20 Image Fixer</h1>
-      <p className='desc'>Website to fix IPL20 Images</p>
+      <h1>IPLT20 Image Fixer and Upscaler</h1>
+      <p className="desc">Website to fix or upscale IPLT20 Images</p>
+      <p className="warning">
+        WARNING: Upscaling uses your GPU power so the higher resolution will
+        take longer time and {<br/>}might slow down your browser during the process.
+      </p>
       <DragAndDrop />
-      <h2 className='footer'>Made by Priyansu Choudhury</h2>
+      <h2 className="footer">Made by Priyansu Choudhury</h2>
     </div>
   );
 }
--- src/DragAndDrop.js
+++ src/DragAndDrop.js
@@ -1,7 +1,12 @@
 import React, { useState } from "react";
+import Upscaler from "upscaler";
+import { x2, x4, x8 } from "@upscalerjs/esrgan-thick";
+const upscaler = new Upscaler();
 
 const DragAndDrop = () => {
   const [url, setUrl] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [modelLoading, setModelLoading] = useState('');
   const handleDragEnter = (e) => {
     e.preventDefault();
     e.stopPropagation();
@@ -41,7 +46,73 @@ const DragAndDrop = () => {
     };
   };
 
-  const handleUrl = (url) => {
+  const handleUpscaleUrl = async (url) => {
+    const img = new Image();
+    img.crossOrigin = "anonymous";
+    setLoading(true);
+    await upscaler
+      .upscale(url)
+      .then((res) => {
+        setLoading(false);
+        img.src = res;
+        img.onload = function () {
+          const canvas = document.createElement("canvas");
+          canvas.width = img.width;
+          canvas.height = img.height;
+          const ctx = canvas.getContext("2d");
+          ctx.drawImage(img, 0, 0);
+          const link = document.createElement("a");
+          link.download = Date.now().toString() + ".jpg";
+          link.href = canvas.toDataURL("image/jpeg");
+          link.click();
+        };
+      })
+      .catch((err) => {
+        setLoading(false);
+        console.log(err);
+      });
+  };
+
+  const handleUpscaleByUrl = async (url, model) => {
+    const up = new Upscaler({
+      model: model,
+    });
+    const img = new Image();
+    img.crossOrigin = "anonymous";
+    setLoading(true);
+    if(model === x2) {
+        setModelLoading('2x');
+    } else if(model === x4) {
+        setModelLoading('4x');
+    } else if(model === x8) {
+        setModelLoading('8x');
+    }
+    await up
+      .upscale(url)
+      .then((res) => {
+        setLoading(false);
+        setModelLoading('');
+        img.src = res;
+        img.onload = function () {
+          const canvas = document.createElement("canvas");
+          canvas.width = img.width;
+          canvas.height = img.height;
+          const ctx = canvas.getContext("2d");
+          ctx.drawImage(img, 0, 0);
+          const link = document.createElement("a");
+          link.download = Date.now().toString() + ".jpg";
+          link.href = canvas.toDataURL("image/jpeg");
+          link.click();
+        };
+      })
+      .catch((err) => {
+        setLoading(false);
+        setModelLoading('');
+        console.log(err);
+      });
+  };
+
+  const handleFixUrl = async (url) => {
     const img = new Image();
     img.crossOrigin = "anonymous";
     img.src = url;
@@ -52,7 +123,7 @@ const DragAndDrop = () => {
       const ctx = canvas.getContext("2d");
       ctx.drawImage(img, 0, 0);
       const link = document.createElement("a");
-      link.download = Date.now().toString()+".jpg";
+      link.download = Date.now().toString() + ".jpg";
       link.href = canvas.toDataURL("image/jpeg");
       link.click();
     };
@@ -65,7 +136,27 @@ const DragAndDrop = () => {
         placeholder="Enter Image Url"
         onChange={(e) => setUrl(e.target.value)}
       />
-      <button onClick={()=>handleUrl(url)}>Submit</button>
+      <button 
+      className="fix"
+      onClick={() => handleFixUrl(url)} disabled={loading}>
+        Fix
+      </button>
+      <br /><br />
+      <button onClick={() => handleUpscaleUrl(url)} disabled={loading}>
+        {loading ? "Upscaling..." : "Normal Upscale"}
+      </button>
+
+      <button onClick={() => handleUpscaleByUrl(url, x2)} disabled={loading}>
+        {modelLoading === '2x' ? "Upscaling..." : "2x Upscale"}
+      </button>
+
+      <button onClick={() => handleUpscaleByUrl(url, x4)} disabled={loading}>
+        {modelLoading === '4x' ? "Upscaling..." : "4x Upscale"}
+      </button>
+
+      <button onClick={() => handleUpscaleByUrl(url, x8)} disabled={loading}>
+        {modelLoading === '8x' ? "Upscaling..." : "8x Upscale"}
+      </button>
 
       <div
         className="drag-drop-zone inside-drag-area"
