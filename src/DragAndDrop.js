6d19e64d222a3e2c6868e8290806987dc3ef7b67
Priyansu67 <25521478+Priyansu67@users.noreply.github.com>
2023-03-20 13:49:23 +0530

    Fixed BUgs

--- src/DragAndDrop.js
+++ src/DragAndDrop.js
@@ -1,11 +1,7 @@
-import React from "react";
-//import axios from "axios";
-//import Jimp from "jimp";
-//const client = axios.create();
+import React, { useState } from "react";
 
 const DragAndDrop = () => {
-  const [base64Image, setBase64Image] = React.useState(null);
-
+  const [url, setUrl] = useState("");
   const handleDragEnter = (e) => {
     e.preventDefault();
     e.stopPropagation();
@@ -22,46 +18,54 @@ const DragAndDrop = () => {
 
   const handleDrop = (e) => {
     e.preventDefault();
-    console.log(e.dataTransfer.files[0]);
-    const name =  + Date.now() + e.dataTransfer.files[0]["name"]
-    console.log(name);
+    const imageFile = e.dataTransfer.files[0];
+    console.log(imageFile);
+    const name = Date.now() + e.dataTransfer.files[0]["name"];
 
     const reader = new FileReader();
-    reader.readAsArrayBuffer(e.dataTransfer.files[0]);
-
-    // Set the onloadend function to handle the file data
-    reader.onloadend = function () {
-      // Get the buffer data from the reader result
-      const bufferData = reader.result;
-
-      // Convert the ArrayBuffer data to a binary string
-      const binaryString = String.fromCharCode.apply(
-        null,
-        new Uint8Array(bufferData)
-      );
-
-      // Convert the binary string to base64 using the Buffer module
-      const base64String = Buffer.from(binaryString, "binary").toString(
-        "base64"
-      );
-
-      setBase64Image(base64String);
-      downloadImage(base64String, name);
+    reader.readAsDataURL(e.dataTransfer.files[0]);
+    reader.onload = function () {
+      const img = new Image();
+      img.src = reader.result;
+      img.onload = function () {
+        const canvas = document.createElement("canvas");
+        canvas.width = img.width;
+        canvas.height = img.height;
+        const ctx = canvas.getContext("2d");
+        ctx.drawImage(img, 0, 0);
+        const link = document.createElement("a");
+        link.download = name;
+        link.href = canvas.toDataURL("image/jpeg");
+        link.click();
+      };
     };
   };
 
-  function downloadImage(base64String, fileName) {
-    const a = document.createElement("a");
-    a.href = `data:image/jpeg;base64,${base64String}`;
-    a.download = fileName;
-    a.click();
-  }
+  const handleUrl = (url) => {
+    const img = new Image();
+    img.crossOrigin = "anonymous";
+    img.src = url;
+    img.onload = function () {
+      const canvas = document.createElement("canvas");
+      canvas.width = img.width;
+      canvas.height = img.height;
+      const ctx = canvas.getContext("2d");
+      ctx.drawImage(img, 0, 0);
+      const link = document.createElement("a");
+      link.download = "image.jpg";
+      link.href = canvas.toDataURL("image/jpeg");
+      link.click();
+    };
+  };
 
   return (
     <>
-      {base64Image && (
-        <img src={`data:image/jpg;base64,${base64Image}`} alt="IPL20" />
-      )}
+      <input
+        type="text"
+        placeholder="Enter Image Url"
+        onChange={(e) => setUrl(e.target.value)}
+      />
+      <button onClick={handleUrl(url)}>Submit</button>
 
       <div
         className="drag-drop-zone inside-drag-area"
